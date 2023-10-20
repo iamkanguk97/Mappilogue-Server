ea32b0e582040f9ca146599910e7ee62d9827779
KANGUK LEE <iamkanguk97@kakao.com>
2023-10-21 01:52:07 +0900

    [feat] 알림 설정 조회하기 API (#6)

--- src/modules/api/user-profile/user-profile.module.ts
+++ src/modules/api/user-profile/user-profile.module.ts
@@ -4,10 +4,14 @@ import { UserProfileService } from './services/user-profile.service';
 import { CustomRepositoryModule } from 'src/modules/core/custom-repository/custom-repository.module';
 import { UserModule } from '../user/user.module';
 import { UserRepository } from '../user/repositories/user.repository';
+import { UserAlarmSettingRepository } from '../user/repositories/user-alarm-setting.repository';
 
 @Module({
   imports: [
-    CustomRepositoryModule.forCustomRepository([UserRepository]),
+    CustomRepositoryModule.forCustomRepository([
+      UserRepository,
+      UserAlarmSettingRepository,
+    ]),
     UserModule,
   ],
   controllers: [UserProfileController],
