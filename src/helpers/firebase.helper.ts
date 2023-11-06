94146d7136c046740702fce6fb3f5949c6dfc04f
KANGUK LEE <iamkanguk97@kakao.com>
2023-11-06 15:37:15 +0900

    [commit] 1106

--- src/helpers/firebase.helper.ts
+++ src/helpers/firebase.helper.ts
@@ -1,10 +1,15 @@
 import * as path from 'path';
 
-export function setFirebaseCredential(dirname: string) {
+/**
+ * @title firebase-admin.json 경로 설정해주는 함수
+ * @param dirname
+ * @returns
+ */
+export function setFirebaseCredential(dirname: string): string {
   switch (process.env.TEMP) {
     case 'dev2':
       return path.join(dirname, '..', 'src/config/firebase-admin.json');
-    case 'dev1':
+    default:
       return path.join(dirname, '..', '..', 'config/firebase-admin.json');
   }
 }
