160743c5a1170048f0c7a1320ef3f2c3edce73b7
KANGUK LEE <iamkanguk97@kakao.com>
2023-11-06 14:18:46 +0900

    [feat] 기록 카테고리 조회 API - 기록 개수 쿼리 추가

--- src/modules/api/mark/repositories/mark-category.repository.ts
+++ src/modules/api/mark/repositories/mark-category.repository.ts
@@ -4,17 +4,31 @@ import { Repository } from 'typeorm';
 import { StatusColumnEnum } from 'src/constants/enum';
 import { MARK_CATEGORY_EMPTY_SEQUENCE } from '../../mark-category/constants/mark-category.constant';
 import * as _ from 'lodash';
+import { MarkEntity } from '../entities/mark.entity';
+import { TMarkCategoryByUserId } from '../../mark-category/types';
 
 @CustomRepository(MarkCategoryEntity)
 export class MarkCategoryRepository extends Repository<MarkCategoryEntity> {
   async selectMarkCategoriesByUserId(
     userId: number,
-  ): Promise<MarkCategoryEntity[]> {
-    return await this.createQueryBuilder()
-      .select(['id', 'title', 'isMarkedInMap'])
-      .where('userId = :userId', { userId })
-      .andWhere('status = :status', { status: StatusColumnEnum.ACTIVE })
-      .orderBy('sequence')
+  ): Promise<TMarkCategoryByUserId[]> {
+    return await this.createQueryBuilder('MC')
+      .select([
+        'MC.id AS id',
+        'MC.title AS title',
+        'MC.isMarkedInMap AS isMarkedInMap',
+        'COUNT(M.id) AS markCount',
+      ])
+      .leftJoin(
+        MarkEntity,
+        'M',
+        'MC.id = M.markCategoryId and M.status = :status',
+        { status: StatusColumnEnum.ACTIVE },
+      )
+      .where('MC.userId = :userId', { userId })
+      .andWhere('MC.status = :status', { status: StatusColumnEnum.ACTIVE })
+      .groupBy('MC.id')
+      .orderBy('MC.sequence')
       .getRawMany();
   }
 
