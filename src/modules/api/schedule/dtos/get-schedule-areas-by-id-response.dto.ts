c9920a141fcf1966722d96e49dc32320bf902021
KANGUK LEE <iamkanguk97@kakao.com>
2023-11-26 01:53:29 +0900

    [FIX] 특정 일정의 장소 조회 API - Response DTO CT 적용 (#59)

--- src/modules/api/schedule/dtos/get-schedule-areas-by-id-response.dto.ts
+++ src/modules/api/schedule/dtos/get-schedule-areas-by-id-response.dto.ts
@@ -1,13 +1,19 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IScheduleAreasById } from '../types';
 
 export class GetScheduleAreasByIdResponseDto {
-  private readonly areas: IScheduleAreasById[];
+  @Exclude() private readonly _areas: IScheduleAreasById[];
 
   private constructor(areas: IScheduleAreasById[]) {
-    this.areas = areas;
+    this._areas = areas;
   }
 
   static of(areas: IScheduleAreasById[]): GetScheduleAreasByIdResponseDto {
     return new GetScheduleAreasByIdResponseDto(areas);
+  }
+
+  @Expose()
+  get areas(): IScheduleAreasById[] {
+    return this._areas;
   }
 }
