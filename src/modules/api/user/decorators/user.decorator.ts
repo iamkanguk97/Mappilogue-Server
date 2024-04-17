3cb986e8edff3b869b2feaa134dd8b09e040dd14
KANGUK LEE <iamkanguk97@kakao.com>
2024-04-17 23:36:43 +0900

    [Fix] UserDecorator 수정

--- src/modules/api/user/decorators/user.decorator.ts
+++ src/modules/api/user/decorators/user.decorator.ts
@@ -1,15 +1,14 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { IRequestWithUserType } from 'src/types/request-with-user.type';
 import { TDecodedUserToken } from '../types';
-import { TPickPrimitive } from 'src/types/pick-primitive.type';
-import { UserEntity } from '../entities/user.entity';
 
-type TUserDecoratorParam = keyof TPickPrimitive<UserEntity>;
+type TUserDecoratorData = keyof TDecodedUserToken;
 
 export const User = createParamDecorator(
-  (data: TUserDecoratorParam, ctx: ExecutionContext): TDecodedUserToken => {
+  (data: TUserDecoratorData, ctx: ExecutionContext) => {
     // data는 UserEntity의 key들이어야함.
     const request = ctx.switchToHttp().getRequest() as IRequestWithUserType;
-    return request.user;
+    const user = request.user;
+    return user[`${data}`] ?? user;
   },
 );
